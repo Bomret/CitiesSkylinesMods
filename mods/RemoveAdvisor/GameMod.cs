7e8d0fe722da95c93ec00abe30b559c6ae5f0b81
Stefan Reichel <stefan@reichelhq.de>
2024-01-13 19:04:10 +0100

    feat: add Remove Advisor mod

---
+++ mods/RemoveAdvisor/GameMod.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using ColossalFramework.UI;
+using ICities;
+using UnityEngine;
+
+namespace Bomret.RemoveAdvisor
+{
+	public sealed class GameMod : LoadingExtensionBase, IUserMod
+	{
+		public string Name { get { return "Remove Advisor"; } }
+		public string Description { get { return "Completely removes the in-game Advisor from the game.\nby Bomret"; } }
+
+		public override void OnLevelLoaded(LoadMode mode)
+		{
+			base.OnLevelLoaded(mode);
+
+			try
+			{
+				RemoveAdvisor();
+			}
+			catch (Exception err)
+			{
+				Debug.Log("[RemoveAdvisor] Error removing Advisor");
+				Debug.LogException(err);
+			}
+		}
+
+		static void RemoveAdvisor()
+		{
+			var advisorButtonObject = GameObject.Find("AdvisorButton");
+			if (advisorButtonObject != null)
+			{
+				Debug.Log("[RemoveAdvisor] Removing Advisor button");
+				var advisorButtonComponent = advisorButtonObject.GetComponent<UIComponent>();
+				UnityEngine.Object.Destroy(advisorButtonComponent.gameObject);
+			}
+			else
+			{
+				Debug.Log("[RemoveAdvisor] Could not find Advisor button");
+			}
+
+			if (TutorialAdvisorPanel.instance != null)
+			{
+				Debug.Log("[RemoveAdvisor] Removing Advisor panel");
+				UnityEngine.Object.Destroy(TutorialAdvisorPanel.instance.gameObject);
+			}
+			else
+			{
+				Debug.Log("[RemoveAdvisor] Could not find Advisor panel");
+			}
+		}
+	}
+}
